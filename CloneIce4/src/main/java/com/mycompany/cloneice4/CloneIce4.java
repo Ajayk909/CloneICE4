d40d2bde419f0e59267370e508bd7ca0958e2f47
91743 <91743@AJAYKHUNTI>
2026-02-06 19:51:21 -0500

    initial commit

---
+++ CloneIce4/src/main/java/com/mycompany/cloneice4/CloneIce4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cloneice4;
+
+/**
+ *
+ * @author 91743
+ */
+public class CloneIce4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
